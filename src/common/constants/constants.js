88679fe43a62a85f407a2b902aac553f1b884023
magluar <aryann.magluyan@oocl.com>
2021-08-17 11:57:29 +0800

    [todo][magluar] add todo group, todo item, and constants

---
+++ src/common/constants/constants.js
@@ -0,0 +1,7 @@
+export const initialToDoList = [
+    {
+        id: "cc53dc26-61b0-406b-99dd-b8825dd2ceec",
+        text: "todo example",
+        done: false
+    },
+];
---
+++ src/features/components/TodoGroup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+import {initialToDoList} from "../../../common/constants/constants"
+
+function TodoGroup() {
+    return (
+        <div>
+           todoItemz
+           <TodoItem/>
+        </div>
+    )
+}
+
+export default TodoGroup
