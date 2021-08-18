6ad49520255a89dbcf7a06a3660d430261b29625
magluar <aryann.magluyan@oocl.com>
2021-08-18 14:31:04 +0800

    [todo][magluar] feat: add api axios

--- src/features/todos/components/ToDoList.js
+++ src/features/todos/components/ToDoList.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
 import TodoForm from './TodoForm'
 import TodoGroup from './TodoGroup'
 import "../../styles/TodoForm.css"
+import { useSelector } from 'react-redux'
+import { selectToDoIds } from '../reducers/todosSlice'
+import { getTodos } from '../../apis/todo'
 
 function ToDoList() {
+    const todoIds = useSelector(selectToDoIds);
+
+    useEffect(() => {
+        getTodos().then((response) => {
+            console.log(response);
+        })
+    }, [])
+
     return (
         <div>
             <h1>TodoList</h1>
