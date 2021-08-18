04d3b49ffe14e8127ee8dc428ce85df6ee7fd151
magluar <aryann.magluyan@oocl.com>
2021-08-18 23:29:59 +0800

    [todo][magluar] refactor: remove unused imports

--- src/features/todos/components/ToDoList.js
+++ src/features/todos/components/ToDoList.js
@@ -3,13 +3,12 @@ import { useEffect } from 'react'
 import TodoForm from './TodoForm'
 import TodoGroup from './TodoGroup'
 import "../../styles/TodoForm.css"
-import { useDispatch, useSelector } from 'react-redux'
-import { selectToDoIds, addTodos } from '../reducers/todosSlice'
+import { useDispatch } from 'react-redux'
+import { addTodos } from '../reducers/todosSlice'
 import { getTodos } from '../../apis/todo'
 
 
 function ToDoList() {
-    const todoIds = useSelector(selectToDoIds);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -19,20 +18,13 @@ function ToDoList() {
     }, [dispatch])
 
     return (
-        // <div>
-        //     <h1>TodoList</h1>
-        //     <TodoGroup/>
-        //     <TodoForm/>
-        // </div>
         <div className="card marginLeft marginRight marginTop">
         <div className="card-header">
           Todo List
         </div>
         <div className="card-body">
-          {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
           <TodoGroup/>
           <TodoForm/>
-          {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
         )
